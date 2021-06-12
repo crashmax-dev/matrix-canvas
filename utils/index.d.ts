d3cc1569811f2a46fb80fa4eb83e2d9780d5c871
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-12 08:22:52 +0000

    Deploy crashmax-dev/matrix-js to github.com/crashmax-dev/matrix-js.git:gh-pages

--- utils/index.d.ts
+++ utils/index.d.ts
@@ -1,2 +1,1 @@
-export { useRender } from './useRender';
 export { randomInt } from './randomInt';
--- utils/useRender.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    interface Window {
-        mozRequestAnimationFrame(e: FrameRequestCallback): void;
-        oRequestAnimationFrame(e: FrameRequestCallback): void;
-        msRequestAnimationFrame(e: FrameRequestCallback): void;
-    }
-}
-export declare const useRender: ((callback: FrameRequestCallback) => number) & typeof requestAnimationFrame;
