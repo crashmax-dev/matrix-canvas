0b4bde0c2d5372394877f55a61461e4c1e87123c
crashmax-dev <sys@crashmax.ru>
2021-10-29 07:32:39 +0000

    Deploying to gh-pages from @ crashmax-dev/canvas-matrix2d@11bb8aad14be33391cc1411f7ac6f3b6838a4dc7 🚀

--- entity.d.ts
+++ entity.d.ts
@@ -12,10 +12,8 @@ export declare class Entity {
     private matrix;
     options: Required<EntityOptions>;
     private flyingEntities;
-    private images;
     private interval;
     constructor(matrix: Matrix, options: EntityOptions | undefined);
-    private loadImages;
     private randomImage;
     start(): void;
     stop(): void;
